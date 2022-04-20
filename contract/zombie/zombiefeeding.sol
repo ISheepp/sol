d264b019198c7e867ccf1d9c9d861d0bf4db71f7
linziyang1106 <810230854@qq.com>
2022-04-20 20:39:07 +0800

    增加猫zombie的合约

--- contract/zombie/zombiefeeding.sol
+++ contract/zombie/zombiefeeding.sol
@@ -3,14 +3,42 @@ pragma solidity ^0.4.19;
 
 import "./zombiefactory.sol";
 
-// 继承自ZombieFactory
+/// 定义接口（为了调用其他合约）
+contract KittyInterface {
+  function getKitty(uint256 _id) external view returns (
+    bool isGestating,
+    bool isReady,
+    uint256 cooldownIndex,
+    uint256 nextActionAt,
+    uint256 siringWithId,
+    uint256 birthTime,
+    uint256 matronId,
+    uint256 sireId,
+    uint256 generation,
+    uint256 genes
+  );
+}
 contract ZombieFeeding is ZombieFactory {
 
-  function feedAndMultiply(uint _zombieId, uint _targetDna) public {
-    // 只有拥有者才能喂养
+  address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+  KittyInterface kittyContract = KittyInterface(ckAddress);
+
+  function feedAndMultiply(uint _zombieId, uint _targetDna, string _species) public {
     require(msg.sender == zombieToOwner[_zombieId]);
     Zombie storage myZombie = zombies[_zombieId];
-    // start here
+    _targetDna = _targetDna % dnaModulus;
+    uint newDna = (myZombie.dna + _targetDna) / 2;
+    if (keccak256(_species) == keccak256("kitty")) {
+      newDna = newDna - newDna % 100 + 99;
+    }
+    _createZombie("NoName", newDna);
   }
-  
+
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    // 由于getKitty的返回值有10个，而我们只需要其中的genes，所以我们只需要获取genes即可
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna, "kitty");
+  }
+
 }
