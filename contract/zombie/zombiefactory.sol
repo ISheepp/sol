1da35fa1083a88158feb93dbbc331489c3762d93
linziyang1106 <810230854@qq.com>
2022-05-06 21:45:50 +0800

    😀格式化code

--- contract/zombie/zombiefactory.sol
+++ contract/zombie/zombiefactory.sol
@@ -6,34 +6,34 @@ import "./ownable.sol";
 // 继承Ownable合约 所有被onlyOwner修饰符修饰的函数都只能被owner调用
 contract ZombieFactory is Ownable {
     // 事件
-    event NewZombie(uint zombieId, string name, uint dna);
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
 
     // 状态变量
-    uint dnaDigits = 16;
-    uint dnaModulus = 10 ** dnaDigits;
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
 
     // 结构体
     struct Zombie {
         string name;
-        uint dna;
+        uint256 dna;
     }
 
     Zombie[] public zombies;
 
-    mapping (uint => address) public zombieToOwner;
-    mapping (address => uint) ownerZombieCount;
+    mapping(uint256 => address) public zombieToOwner;
+    mapping(address => uint256) ownerZombieCount;
 
     // 创建zombie
-    function _createZombie(string _name, uint _dna) private {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+    function _createZombie(string _name, uint256 _dna) private {
+        uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
         zombieToOwner[id] = msg.sender;
         ownerZombieCount[msg.sender]++;
         NewZombie(id, _name, _dna);
     }
 
     // 生成一个16位的DNA
-    function _generateRandomDna(string _str) private view returns (uint) {
-        uint rand = uint(keccak256(_str));
+    function _generateRandomDna(string _str) private view returns (uint256) {
+        uint256 rand = uint256(keccak256(_str));
         return rand % dnaModulus;
     }
 
@@ -41,8 +41,7 @@ contract ZombieFactory is Ownable {
     function createRandomZombie(string _name) public {
         // 每个用户只能拥有一个zombie
         require(ownerZombieCount[msg.sender] == 0);
-        uint randDna = _generateRandomDna(_name);
+        uint256 randDna = _generateRandomDna(_name);
         _createZombie(_name, randDna);
     }
-
 }
--- contract/zombie/zombiehelper.sol
+++ contract/zombie/zombiehelper.sol
@@ -5,33 +5,41 @@ pragma solidity ^0.4.19;
 import "./zombiefeeding.sol";
 
 contract ZombieHelper is ZombieFeeding {
-
-  modifier aboveLevel(uint _level, uint _zombieId) {
-    require(zombies[_zombieId].level >= _level);
-    _;
-  }
-
-  function changeName(uint _zombieId, string _newName) external aboveLevel(2, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
-    zombies[_zombieId].name = _newName;
-  }
-
-  function changeDna(uint _zombieId, uint _newDna) external aboveLevel(20, _zombieId) {
-    require(msg.sender == zombieToOwner[_zombieId]);
-    zombies[_zombieId].dna = _newDna;
-  }
-
-  function getZombiesByOwner(address _owner) external view returns(uint[]) {
-    uint[] memory result = new uint[](ownerZombieCount[_owner]);
-    // 在这里开始
-    uint conunter = 0;
-    for(uint i = 0; i < zombies.length; i++) {
-      if (zombieToOwner[i] == _owner) {
-        result[counter] = i;
-        counter++;
-      }
+    modifier aboveLevel(uint256 _level, uint256 _zombieId) {
+        require(zombies[_zombieId].level >= _level);
+        _;
     }
-    return result;
-  }
 
+    function changeName(uint256 _zombieId, string _newName)
+        external
+        aboveLevel(2, _zombieId)
+    {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+
+    function changeDna(uint256 _zombieId, uint256 _newDna)
+        external
+        aboveLevel(20, _zombieId)
+    {
+        require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    }
+
+    function getZombiesByOwner(address _owner)
+        external
+        view
+        returns (uint256[])
+    {
+        uint256[] memory result = new uint256[](ownerZombieCount[_owner]);
+        // 在这里开始
+        uint256 conunter = 0;
+        for (uint256 i = 0; i < zombies.length; i++) {
+            if (zombieToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
 }
